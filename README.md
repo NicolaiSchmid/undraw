ee845ea165257b26b11a8ad79766cccaf33d50e5
Nicolai Schmid <nicolai@schmid.uno>
2020-06-27 19:55:42 +0200

    feat: new version without images and only links to the graphics

--- README.md
+++ README.md
@@ -1,3 +1,19 @@
 [Undraw graphics](https://undraw.co)
 
+This is a JS exported collection of links pointing to undraw hosted images.
+
+Example:
+
+```javascript
+{
+  coding: 'https://42f2671d685f51e10fc6-b9fcecea3e50b3b59bdc28dead054ebc.ssl.cf5.rackcdn.com/illustrations/coding_6mjf.svg',
+  selfieTime: 'https://42f2671d685f51e10fc6-b9fcecea3e50b3b59bdc28dead054ebc.ssl.cf5.rackcdn.com/illustrations/selfie_time_cws4.svg',
+  webDevices: 'https://42f2671d685f51e10fc6-b9fcecea3e50b3b59bdc28dead054ebc.ssl.cf5.rackcdn.com/illustrations/web_devices_ad58.svg',
+  eatingTogether: 'https://42f2671d685f51e10fc6-b9fcecea3e50b3b59bdc28dead054ebc.ssl.cf5.rackcdn.com/illustrations/eating_together_tjhx.svg',
+  ...
+}
+```
+
+Because undraw doesn't allow me to publish the images directly this only serves as a pointer to the graphics, while you could combine this module with a [remote-webpack-plugin](https://github.com/roneyrao/remote-webpack-plugin) that fetches the images at build time to maintain a legal usage of the graphics and a developer-friendly experience
+
 https://www.npmjs.com/package/@nicolaischmid/undraw
--- src/index.js
+++ src/index.js
@@ -1,16 +1,54 @@
 import fetch from "node-fetch";
 import qs from "qs";
+import camelcase from "camelcase";
+import { existsSync } from "fs";
+import { stats, mkdir, writeFile } from "fs/promises";
+
+function prepareObject(illustrations) {
+  const obj = {};
+  illustrations.forEach((ill) => {
+    obj[camelcase(ill.title)] = ill.image;
+  });
+
+  return obj;
+}
+
+function individualFile() {}
+
+async function write(images) {
+  const dir = "./dist";
+
+  if (!existsSync(dir)) {
+    await mkdir(dir);
+  }
+
+  await writeFile(`${dir}/index.json`, JSON.stringify(images));
+
+  await Promise.all(
+    Object.keys(images).map(async (key) => {
+      return writeFile(
+        `${dir}/${key}.js`,
+        `module.exports = "${images[key]}";`
+      );
+    })
+  );
+}
 
 const query = async (page) => {
   const response = await fetch(
     `https://undraw.co/api/illustrations?${qs.stringify({ page })}`
   ).then((res) => res.json());
 
-  response.illustrations.map((ill) => process.stdout.write(`${ill.image}\n`));
+  if (!response.hasMore) return prepareObject(response.illustrations);
 
-  if (!response.hasMore) return response.illustrations;
-
-  return [...response.illustrations, ...(await query(response.nextPage))];
+  return {
+    ...prepareObject(response.illustrations),
+    ...(await query(response.nextPage)),
+  };
 };
 
-query(1);
+(async () => {
+  const images = await query(1);
+  console.log(images);
+  await write(images);
+})();
